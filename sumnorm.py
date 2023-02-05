655fc40bee18a17a3253dae90ff427a8978fe94c
zero-pytagoras <91202914+zero-pytagoras@users.noreply.github.com>
2023-02-05 09:06:54 +0200

    Update sumnorm.py

--- sumnorm.py
+++ sumnorm.py
@@ -1,10 +1,19 @@
-
+import sys
+import pandas as pd
+import numpy as np
+    
+    
 def sum_norm(df):
-    import pandas as pd
-    import numpy as np
+    '''
+    describe the func
+    '''
     n=df.shape[0]
     sumcol = df.sum(axis=0)
     sumcol.to_numpy
     rep_sum = np.tile(sumcol, (n,1))
     mat_norm = df/rep_sum
     return mat_norm
+
+
+if __name__ == "__main__":
+    sum_norm(sys.argv[1:])
