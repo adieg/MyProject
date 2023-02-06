61a555eae93bfc46f94292951e5228171d8e9282
Dshisher <dshisher@gmail.com>
2023-02-06 12:12:03 +0200

    added another type of normalization.

--- sumnorm.py
+++ sumnorm.py
@@ -7,13 +7,40 @@ def sum_norm(df):
     '''
     This function calculates the sum-normalized values (fractions) of the dataframe over columns
     '''
-    n=df.shape[0]
+    n = df.shape[0]
     sumcol = df.sum(axis=0)
     sumcol.to_numpy
-    rep_sum = np.tile(sumcol, (n,1))
+    rep_sum = np.tile(sumcol, (n, 1))
     mat_norm = df/rep_sum
     return mat_norm
 
 
+def scaling_range_norm(df, val_min=0, val_max=0):
+    """
+    This function calculates the scales-normalized values of the dataframe over columns
+    """
+    if val_min == 0 and val_max == 0:
+        val_min = df.min()
+        val_max = df.max()
+    df_scaling = (df - val_min) / (val_max - val_min)
+
+    return df_scaling
+
+
+def main(df, normalization_type="scaling_range"):
+    """
+    This function send calculation to the right normalization type:
+    """
+    if normalization_type == "sum":
+        result = sum_norm(df)
+    elif normalization_type == "scaling_range":
+        result = scaling_range_norm(df)
+    else:
+        print("Error in normalization type! Calculating sum normalization.")
+        result = sum_norm(df)
+
+    return result
+
+
 if __name__ == "__main__":
-    sum_norm(sys.argv[1:])
+    main(sys.argv[1:])
