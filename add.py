1569b9bb515c3ea0005f5bff965a6598042decc7
ilayni <ilayorion@gmail.com>
2023-02-07 10:47:09 +0200

    pep8 formatting adding spaces

--- add.py
+++ add.py
@@ -1,9 +1,10 @@
 import sumnorm
 import pandas as pd
 import numpy as np
+
 data = {
-  "V1": [30, 50, 20],
-  "V2": [10, 45, 45]
+    "V1": [30, 50, 20],
+    "V2": [10, 45, 45]
 }
 
 df1 = pd.DataFrame(data)
