9d6137f318832ed4c21c86e8d4da163f85b9108d
ilayni <ilayorion@gmail.com>
2023-02-09 19:10:11 +0200

    2 more fixes for pep8, added black for pep8 test to requirments.txt

--- add.py
+++ add.py
@@ -2,10 +2,7 @@ import sumnorm
 import pandas as pd
 import numpy as np
 
-data = {
-    "V1": [30, 50, 20],
-    "V2": [10, 45, 45]
-}
+data = {"V1": [30, 50, 20], "V2": [10, 45, 45]}
 
 df1 = pd.DataFrame(data)
 
--- sumnorm.py
+++ sumnorm.py
@@ -1,17 +1,17 @@
 import sys
 import pandas as pd
 import numpy as np
-    
-    
+
+
 def sum_norm(df):
-    '''
+    """
     This function calculates the sum-normalized values (fractions) of the dataframe over columns
-    '''
-    n=df.shape[0]
+    """
+    n = df.shape[0]
     sumcol = df.sum(axis=0)
     sumcol.to_numpy
-    rep_sum = np.tile(sumcol, (n,1))
-    mat_norm = df/rep_sum
+    rep_sum = np.tile(sumcol, (n, 1))
+    mat_norm = df / rep_sum
     return mat_norm
 
 
